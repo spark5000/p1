53df8c2bf06377a8a29e45d3cbf2f8ad43afa57b
spark5000 <oddysey1215@hotmail.com>
2016-02-11 06:19:16 -0500

    update

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<h3>Hello! This is p1!</h3>
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Samuel Park P1 - CSCI E-15 Portfolio</title>
+		<link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="wrapper">
+			<h2>Samuel Park</h2>
+
+		</div>
+
+	</body>
+
+</html>
